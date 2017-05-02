1343ce72d656030254573b4468e5212c70f0d51f
David Thomason <dlthomas@us.ibm.com>
2017-05-01 21:10:29 -0400

    Added validation errors on home page

--- app/controllers/git_hub_contributors_controller.rb
+++ app/controllers/git_hub_contributors_controller.rb
@@ -1,12 +1,20 @@
 class GitHubContributorsController < ApplicationController
   def home
+    @user = GitHubContributor.new
   end
 
   def show
-    user = GitHubContributor.new(username: params[:username])
-    if user.valid?
-      @username = user.username
-      @profile = user.get_profile
+    puts params
+    @user = GitHubContributor.new(git_hub_contributor_params)
+    if @user.valid?
+      @username = @user.username
+      @profile = @user.github_profile
+    else
+      render action: 'home'
     end
+  end
+
+  def git_hub_contributor_params
+    params.require(:git_hub_contributor).permit(:username)
   end
 end
--- app/models/git_hub_contributor.rb
+++ app/models/git_hub_contributor.rb
@@ -2,14 +2,19 @@ class GitHubContributor
   include ActiveModel::Model
   attr_accessor :username
   validates_presence_of :username
+  validate :github_profile
 
-  def get_profile
+  def github_profile
     profile_url = "https://github.com/#{self.username}"
-    doc = Nokogiri::HTML(open profile_url)
-    {
-        url: profile_url,
-        avatar: doc.css('img.avatar')[0].to_s,
-        contribution_graph: doc.css('.js-contribution-graph')[0].to_s
-    }
+    begin
+      doc = Nokogiri::HTML(open profile_url)
+      {
+          url: profile_url,
+          avatar: doc.css('img.avatar')[0].to_s,
+          contribution_graph: doc.css('.js-contribution-graph')[0].to_s
+      }
+    rescue OpenURI::HTTPError => e
+      errors.add :username, "Couldn't find a GitHub profile for this username."
+    end
   end
 end
