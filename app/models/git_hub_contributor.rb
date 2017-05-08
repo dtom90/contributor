ac81c3b165596176e944a69b4698f6b2e3436723
David Thomason <dlthomas@us.ibm.com>
2017-05-07 23:16:07 -0400

    Added average and total over last week, month, and year graph

--- app/models/git_hub_contributor.rb
+++ app/models/git_hub_contributor.rb
@@ -11,18 +11,35 @@ class GitHubContributor
     profile_url = "https://github.com/#{self.username}"
     begin
       doc = Nokogiri::HTML(open profile_url)
+      
       contributions = {}
       doc.css('rect.day').each do |day|
         contributions[day['data-date']] = day['data-count']
       end
+
+      last_week_total = sum(contributions, 7)
+      last_month_total = sum(contributions, 30)
+      last_year_total = sum(contributions, 0)
+      
       @profile = {
           url: profile_url,
           avatar: doc.css('img.avatar')[0].to_s,
           contribution_graph: doc.css('.js-contribution-graph')[0].to_s,
-          contributions: contributions
+          contributions: contributions,
+          last_week_total: last_week_total,
+          last_month_total: last_month_total,
+          last_year_total: last_year_total,
+          total_days: contributions.keys.length
       }
     rescue OpenURI::HTTPError => e
       errors.add :username, "Couldn't find a GitHub profile for this username."
     end
   end
+  
+  private
+  
+  def sum(contributions, num_days)
+    contributions.slice(*contributions.keys[-num_days..-1]).values.map{|c| c.to_i}.inject(0, :+)
+  end
+  
 end
