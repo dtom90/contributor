c2d064cd5a0d6b0c14898b504f107c3458da5444
David Thomason <dlthomas@us.ibm.com>
2017-05-09 18:44:53 -0400

    Working JS in test

--- spec/features/github_contributor_searches_spec.rb
+++ spec/features/github_contributor_searches_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.feature 'GitHub Contributor Searches', type: :feature do
-  it 'gets the home page' do
+  it 'gets the home page', js: true do
     visit '/'
     expect(page).to have_content 'GitHub Contributor'
     expect(page).to have_content 'Username'
     expect(page).to have_button 'Get Contributions'
     
-    # click_button 'Get Contributions'
-    # expect(page).to have_text("can't be blank")
+    click_button 'Get Contributions'
+    expect(page).to have_text("can't be blank")
   end
 end
